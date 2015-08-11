64bdcc33a1f69a599f108e264c30709e315cf8c8
Iñigo Beitia <luckybeitia@gmail.com>
2015-08-11 13:00:57 +0200

    Logout works.

--- mobile/app/scripts/controllers/tab.account.controller.js
+++ mobile/app/scripts/controllers/tab.account.controller.js
@@ -1,6 +1,11 @@
 'use strict';
 
 angular.module('helix.controllers')
-  .controller('AccountCtrl', function ($scope) {
+  .controller('AccountCtrl', function ($scope, Auth, $location) {
     console.log('AccountCtrl');
+
+    $scope.logout = function() {
+      Auth.logout();
+      $location.path('/login');
+    };
   });
